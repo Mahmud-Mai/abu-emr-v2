63509e641da4ce2bb27fbfea651081ffb99d73b8
Mahmud Mai <mahmudx00@gmail.com>
2023-11-03 10:10:08 +0100

    Prep for revert 2

--- routes/api/api.js
+++ routes/api/api.js
@@ -1,29 +1,27 @@
-const express = require("express");
+const express = require('express');
 
 const api = express.Router();
 
-const db = require("../../utils/db.js");
+const db = require('../../utils/db');
 
-api.get("/doctors", (req, res) => {
-  const sql =
-    "SELECT first_name, last_name, email, specialisation FROM doctors";
+api.get('/doctors', (req, res) => {
+    const sql = "SELECT first_name, last_name, email, specialisation FROM doctors";
 
-  db.query(sql, (err, result) => {
-    if (err) console.log(err);
+    db.query(sql, (err, result) => {
+        if(err) console.log(err);
 
-    res.json(result);
-  });
+        res.json(result);
+    });
 });
 
-api.get("/patients", (req, res) => {
-  const sql =
-    "SELECT first_name, last_name, email, address, phone_no FROM patient";
+api.get('/patients', (req, res) => {
+    const sql = "SELECT first_name, last_name, email, address, phone_no FROM patient";
 
-  db.query(sql, (err, result) => {
-    if (err) console.log(err);
+    db.query(sql, (err, result) => {
+        if(err) console.log(err);
 
-    res.json(result);
-  });
+        res.json(result);
+    });
 });
 
 // api.get();
